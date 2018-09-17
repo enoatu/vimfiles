edd5450d73eaf4f84368a63e759488728b2d1f1e
enoatu <acro1452@gmail.com>
2018-09-17 18:34:10 +0900

    vimprocのnot foundエラーを修正

--- install.sh
+++ install.sh
@@ -3,4 +3,13 @@ rm -rf ~/.cache/dein ~/.cache/neocomplcache ~/.cache/neosnippet
 cd ~/vimfiles
 unlink ~/.vimrc
 ln -s ~/vimfiles/.vimrc ~/.vimrc
+vim +:q
+mkdir -p ~/.cache/dein/.cache/.vimrc/.dein/lib
+cd ~/.cache/dein/.cache/.vimrc/.dein/lib
+git clone https://github.com/Shougo/vimproc.vim.git
+cd vimproc.vim
+make
+unlink ../vimproc_linux64.so
+ln -s vimproc.vim/lib/vimproc_linux64.so ..
 
+echo "\e[30;42;1msuccess\e[m"
