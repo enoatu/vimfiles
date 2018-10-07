01f68458d95e9b68f2fe2ee221a810da16dd36dd
enoatu <acro1452@gmail.com>
2018-10-07 23:04:02 +0900

    dotfiles対応

--- install.sh
+++ install.sh
@@ -1,18 +1,40 @@
 #!/usr/bin/env bash
 set -eu
-rm -rf ~/vimfiles/dein
-cd ~/vimfiles
+
 if [ -e ~/.vimrc ]; then
-    unlink ~/.vimrc
+    rm ~/.vimrc
 fi
-ln -s ~/vimfiles/.vimrc ~/.vimrc
+
+if [ -e ~/vimfiles ]; then
+    cd ~/vimfiles
+    ln -s ~/vimfiles/vimrc ~/.vimrc
+elif [ -e ~/dotfiles/vimfiles ]; then
+    cd ~/dotfiles/vimfiles
+    sed '/let s:dein_dir/c\let s:dein_dir = expand('~/dotfiles/vimfiles/dein') "@@dotfiles' vimrc
+    ln -s ~/dotfiles/vimfiles/vimrc ~/.vimrc
+else
+    printf "\e[37;41;1m Please change current directory\e[m\n"
+    return
+fi
+
+if [ -e dein ]; then
+    rm -rf dein
+fi
+
 vim +:q
-mkdir -p ~/vimfiles/dein/.cache/.vimrc/.dein/lib
-cd ~/vimfiles/dein/.cache/.vimrc/.dein/lib
+mkdir -p dein/.cache/.vimrc/.dein/lib
+cd dein/.cache/.vimrc/.dein/lib
 git clone https://github.com/Shougo/vimproc.vim.git
 cd vimproc.vim
 make
-ln -s vimproc.vim/lib/vimproc_mac.so ..
-ln -s vimproc.vim/lib/vimproc_linux64.so .. #TODO
 
-printf "\e[30;42;1msuccess\e[m\n"
+if [ -e ./lib/vimproc_linux64.so ]; then
+    ln -s vimproc.vim/lib/vimproc_linux64.so ..
+elif [ -e ./lib/vimproc_mac.so ]; then
+    ln -s vimproc.vim/lib/vimproc_mac.so .. #TODO
+else
+    printf "\e[37;41;1m Couldn't find vimproc.so \e[m\n"
+    return
+fi
+
+printf "\e[30;42;1mvim setup completed\e[m\n"
