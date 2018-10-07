edf7a73e6a1056cafe5088094407d7024dcdd14d
enoatu <acro1452@gmail.com>
2018-10-07 19:35:26 +0900

    vimfile内にcacheを移動

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 set -eu
-rm -rf ~/.cache/dein ~/.cache/neocomplcache ~/.cache/neosnippet
+rm -rf ~/vimfiles/dein
 cd ~/vimfiles
 if [ -e ~/.vimrc ]; then
     unlink ~/.vimrc
 fi
 ln -s ~/vimfiles/.vimrc ~/.vimrc
 vim +:q
-mkdir -p ~/.cache/dein/.cache/.vimrc/.dein/lib
-cd ~/.cache/dein/.cache/.vimrc/.dein/lib
+mkdir -p ~/vimfiles/dein/.cache/.vimrc/.dein/lib
+cd ~/vimfiles/dein/.cache/.vimrc/.dein/lib
 git clone https://github.com/Shougo/vimproc.vim.git
 cd vimproc.vim
 make
 ln -s vimproc.vim/lib/vimproc_mac.so ..
-#ln -s vimproc.vim/lib/vimproc_linux64.so ..
+ln -s vimproc.vim/lib/vimproc_linux64.so .. #TODO
 
 printf "\e[30;42;1msuccess\e[m\n"
