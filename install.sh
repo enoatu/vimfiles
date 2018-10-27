e027b91c5753ae23389f7b2bf87fd7d06ce13279
enoatu <acro1452@gmail.com>
2018-10-28 01:18:35 +0900

    関数化

--- install.sh
+++ install.sh
@@ -1,39 +1,53 @@
 #!/usr/bin/env bash
 set -eux
 
-if [ ! -e ./vimrc ]; then
-    printf "\e[37;41;1m Please change current directory \e[m\n"
-    exit
-fi
+main () {
+    directoryCheck
+    setLink
+    setupVimProc
 
-if [ -e dein ]; then
-    rm -rf dein
-fi
+    printf "\e[30;42;1mvim setup completed\e[m\n"
 
-if [ -e ~/vimfiles ]; then
-    ln -sf ~/vimfiles/vimrc ~/.vimrc
-    ln -sf ~/vimfiles/dein.toml ~/.dein.toml
-fi
+    exit 0
+}
 
-printf "\e[37;44;5mvim open ...\e[m\n"
-vim +:q
-printf "\e[37;44;5mvim close\e[m\n"
+directoryCheck () {
+    if [ ! -e ./vimrc -o ! -e ./dein.toml ]; then
+        printf "\e[37;41;1m Please change current directory \e[m\n"
+        exit 1
+    fi
+}
 
-mkdir -p dein/.cache/.vimrc/.dein/lib
-cd ./dein/.cache/.vimrc/.dein/lib
-git clone https://github.com/Shougo/vimproc.vim.git
-cd vimproc.vim
-make
-printf "\e[30;42;1mvim setup completed\e[m\n"
-if [ -e ./lib/vimproc_linux64.so ]; then
-    ln -s vimproc.vim/lib/vimproc_linux64.so ..
-elif [ -e ./lib/vimproc_mac.so ]; then
-    ln -s vimproc.vim/lib/vimproc_mac.so .. #TODO
-else
-    printf "\e[37;41;1m Couldn't find vimproc.so \e[m\n"
-    exit
-fi
+setLink () {
+    if [ -e dein ]; then
+        rm -rf dein
+    fi
 
-printf "\e[30;42;1mvim setup completed\e[m\n"
+    if [ -e ~/vimfiles ]; then
+        ln -sf ~/vimfiles/vimrc ~/.vimrc
+        ln -sf ~/vimfiles/dein.toml ~/.dein.toml
+    fi
 
-exit 0
+    printf "\e[37;44;5mvim open ...\e[m\n"
+    vim +:q
+    printf "\e[37;44;5mvim close\e[m\n"
+}
+
+setupVimProc () {
+    mkdir -p dein/.cache/.vimrc/.dein/lib
+    cd ./dein/.cache/.vimrc/.dein/lib
+    git clone https://github.com/Shougo/vimproc.vim.git
+    cd vimproc.vim
+    make
+    if [ -e ./lib/vimproc_linux64.so ]; then
+        ln -s vimproc.vim/lib/vimproc_linux64.so ..
+    elif [ -e ./lib/vimproc_mac.so ]; then
+        ln -s vimproc.vim/lib/vimproc_mac.so .. #TODO
+    else
+        printf "\e[37;41;1m Couldn't find vimproc.so \e[m\n"
+        exit
+    fi
+}
+
+main
+
